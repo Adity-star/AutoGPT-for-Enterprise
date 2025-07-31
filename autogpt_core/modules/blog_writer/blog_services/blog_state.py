4c63a6c922107f20cb9ec8ce409a8a6aa38495a2
Aditya Akuskar <adityaakuskar123@gmail.com>
2025-07-31 23:20:04 +0530

    fix blog draft node

--- autogpt_core/modules/blog_writer/blog_services/blog_state.py
+++ autogpt_core/modules/blog_writer/blog_services/blog_state.py
@@ -6,6 +6,6 @@ class BlogWriterAgentState(BaseModel):
     research_summary: Optional[str] = None         
     blog_title: Optional[str] = None              
     blog_draft: Optional[str] = None              
-    seo_keywords: Optional[List[str]] = None      
+    keywords: Optional[List[str]] = None      
     meta_description: Optional[str] = None         
     suggestions: Optional[List[str]] = None        
